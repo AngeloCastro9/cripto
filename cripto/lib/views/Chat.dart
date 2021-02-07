4c1ad3e2ef5b6ff40deacb9125467e6495bfab11
Angelo <castromatheus060@gmail.com>
2021-02-07 14:12:33 -0300

    add page chat and contact

---
+++ cripto/lib/views/Chat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Chat extends StatefulWidget {
+  @override
+  _AbaConversasState createState() => _AbaConversasState();
+}
+
+class _AbaConversasState extends State<Chat> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Conversas"),
+    );
+  }
+}
---
+++ cripto/lib/views/Contact.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Contact extends StatefulWidget {
+  @override
+  _AbaContatosState createState() => _AbaContatosState();
+}
+
+class _AbaContatosState extends State<Contact> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Contatos"),
+    );
+  }
+}
