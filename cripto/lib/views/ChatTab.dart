5907d0d2edf7ca720bf259d60ca954b0ca55c9a1
Angelo <castromatheus060@gmail.com>
2021-02-21 19:51:52 -0300

    Fix Chat Tab

--- cripto/lib/views/ChatTab.dart
+++ cripto/lib/views/ChatTab.dart
@@ -1,44 +1,57 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:cripto/model/Chats.dart';
+import 'package:cripto/model/Chat.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cripto/model/User.dart';
 
-class Chat extends StatefulWidget {
+class ChatTab extends StatefulWidget {
   @override
-  _ChatState createState() => _ChatState();
+  _ChatTabState createState() => _ChatTabState();
 }
 
-class _ChatState extends State<Chat> {
-  List<Chats> _chatList = List();
+class _ChatTabState extends State<ChatTab> {
+  List<Chat> _listChat = List();
   final _controller = StreamController<QuerySnapshot>.broadcast();
   Firestore db = Firestore.instance;
-  String _userIdLogged;
+  String _idUserLogged;
 
   @override
   void initState() {
     super.initState();
     _recoverUserData();
+
+    Chat chat = Chat();
+    chat.name = "only test";
+    chat.message = "test";
+
+    _listChat.add(chat);
   }
 
-  Stream<QuerySnapshot> _addListenerChat() {
+  Stream<QuerySnapshot> _addListenerChats() {
     final stream = db
         .collection("chats")
-        .document(_userIdLogged)
+        .document(_idUserLogged)
         .collection("last_chat")
         .snapshots();
 
-    stream.listen((datas) {
-      _controller.add(datas);
+    stream.listen((data) {
+      _controller.add(data);
     });
   }
 
   _recoverUserData() async {
     FirebaseAuth auth = FirebaseAuth.instance;
     FirebaseUser loggedUser = await auth.currentUser();
-    _userIdLogged = loggedUser.uid;
+    _idUserLogged = loggedUser.uid;
 
-    _addListenerChat();
+    _addListenerChats();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.close();
   }
 
   @override
@@ -62,46 +75,56 @@ class _ChatState extends State<Chat> {
           case ConnectionState.active:
           case ConnectionState.done:
             if (snapshot.hasError) {
-              return Text("Erro ao carregar os datas!");
+              return Text("Erro ao carregar os data!");
             } else {
               QuerySnapshot querySnapshot = snapshot.data;
 
               if (querySnapshot.documents.length == 0) {
                 return Center(
                   child: Text(
-                    "Você não tem nenhuma mensagem ainda :( ",
+                    "Você ainda não tem nenhuma message :( ",
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
                 );
               }
 
               return ListView.builder(
-                  itemCount: _chatList.length,
+                  // itemCount: _listChat.length,
+                  itemCount: querySnapshot.documents.length,
                   itemBuilder: (context, indice) {
                     List<DocumentSnapshot> chats =
                         querySnapshot.documents.toList();
                     DocumentSnapshot item = chats[indice];
 
-                    String urlImage = item["photoPath"];
-                    String typeMessage = item["typeMessage"];
+                    String photoPath = item["photoPath"];
+                    String type = item["typeMessage"];
                     String message = item["message"];
                     String name = item["name"];
+                    String recipientId = item["recipientId"];
+
+                    User user = User();
+                    user.name = name;
+                    user.urlImage = photoPath;
+                    user.userId = recipientId;
 
                     return ListTile(
+                      onTap: () {
+                        Navigator.pushNamed(context, "/messages",
+                            arguments: user);
+                      },
                       contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
                       leading: CircleAvatar(
                         maxRadius: 30,
                         backgroundColor: Colors.grey,
                         backgroundImage:
-                            urlImage != null ? NetworkImage(urlImage) : null,
+                            photoPath != null ? NetworkImage(photoPath) : null,
                       ),
                       title: Text(
                         name,
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 16),
                       ),
-                      subtitle: Text(
-                          typeMessage == "text" ? message : "Imagem...",
+                      subtitle: Text(type == "text" ? message : "Imagem...",
                           style: TextStyle(color: Colors.grey, fontSize: 14)),
                     );
                   });
